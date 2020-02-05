5dec46f5ef1954ebfe071170a291d175a324d023
olivier-crozet <olivier.crozet@laplateforme.io>
2020-02-05 11:51:07 +0100

    creation class user

---
+++ user.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 
+ */
+class user 
+{
+	
+	private $id = 666;
+	public $login = "jesus";
+	public $email = "paradis@dieu";
+	public $firstname = "christ";
+	public $lastname = "jesuschrist";
+	function __construct(argument)
+	{
+		# code...
+	}
+}
+
+?>
