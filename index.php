a253b6cddbf55a4083616cb8ffda70c891798c40
olivier-crozet <olivier.crozet@laplateforme.io>
2020-02-05 19:09:02 +0100

    insertion ok en 9H :/

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+
+session_start();
+
+include("user.php");
+$pd = new user();
+
+
+               //si on clique sur la connexion
+ if (!empty($_POST['formdeconexion'])) 
+    {   	
+    unset ( $_SESSION ['id'] );
+    unset ($_SESSION['login']);	
+$erreur="<p> class='codeerreur'>vous n'etes pas connecté !</p>";
+    }
+
+?>
+
+<DOCTYPE html>
+
+	<body>
+		<form method="post" action="">
+			<?php
+			
+if (!empty($_POST["submit"])) 
+{
+
+      if (!empty($_POST['login']) && !empty($_POST['password'])) 
+      {
+      	$login = htmlspecialchars($_POST['login']);
+       $email = htmlspecialchars($_POST['email']);
+       $firstname = htmlspecialchars($_POST['firstname']);
+       $lastname = htmlspecialchars($_POST['lastname']);
+       $password = sha1($_POST['password']);
+
+       $pd -> register($login,$password,$firstname,$lastname,$email);
+   }
+else
+  {
+    echo "tous les champs doivent etre complétés !";
+  }
+
+}
+
+?>
+ <table class="inputconexion" >
+          <tr>
+              <td>
+                <label  for="login"> login :</label>
+              </td>
+              <td>
+                <input type="text" name="login" placeholder="ecrire votre pseudo" value=""><!--php pour laisser le text dans l'input-->
+              </td>
+          </tr>
+           <tr>
+              <td>
+                <label  for="email"> mail :</label>
+              </td>
+              <td>
+                <input type="mail" name="email" placeholder="mail" value="">
+              </td>
+          </tr>
+          <tr>
+              <td>
+                <label  for="firstname"> prenom :</label>
+              </td>
+              <td>
+                <input type="text" name="firstname" placeholder="prenom" value="">
+              </td>
+          </tr>
+           <tr>
+              <td>
+                <label  for="lastname"> nom :</label>
+              </td>
+              <td>
+                <input type="text" name="lastname" placeholder="nom" value="">
+              </td>
+          </tr>
+
+          <tr>
+               <td>
+                <label  for="passwordconect">mot de passe :</label>
+              </td>
+              <td>
+                <input type="password" name="password" placeholder="ecrire votre mot de passe">
+              </td>
+          </tr>    
+        </table>
+        <br/>
+                <input class="butonconexion" type="submit" name="submit" value="CONNEXION"/>  
+        <br/><br/>
+        		<input class="butonconexion2" type="submit" name="submit" value="se deconnecté"/>       		   
+          </form>
+        
+        </table>
+
+    </body>
+</html>
--- user.php
+++ user.php
@@ -6,15 +6,38 @@
 class user 
 {
 	
-	private $id = 666;
-	public $login = "jesus";
-	public $email = "paradis@dieu";
-	public $firstname = "christ";
-	public $lastname = "jesuschrist";
-	function __construct(argument)
+	private $id = "";
+	public $login = "";
+	public $email = "";
+	public $firstname = "";
+	public $lastname = "";
+	
+
+
+	function register($login,$password,$email,$firstname,$lastname)
 	{
-		# code...
-	}
+
+
+                  $connexion=mysqli_connect("localhost","root","","bddclass");
+                  $reqdoublon = "SELECT login FROM `user` where login=\"$login\";";
+                  $req=mysqli_query($connexion,$reqdoublon);                 
+              $retour=mysqli_num_rows($req);
+
+                           if($retour==0)
+                           {                 
+                           echo "tamere";           
+                            $requete="INSERT INTO user(login,password,email,firstname,lastname)
+                            VALUES (\"$login\",\"$password\",\"$email\",\"$firstname\",\"$lastname\")";                
+                            $inser= mysqli_query($connexion, $requete);
+                             
+                          } 
+                          else
+                          {
+                            echo "ce login existe deja !";
+                          }  
+    	}
+     	
 }
+
 
 ?>
